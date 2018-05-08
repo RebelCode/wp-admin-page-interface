2b3774ba76ec8968f0fa9e75f18a58cff878c89b
Anton Ukhanev <xedin.unknown@gmail.com>
2018-05-08 11:18:14 +0200

    Brought tests up to standard

--- test/unit/PageAwareInterfaceTest.php
+++ test/unit/PageAwareInterfaceTest.php
@@ -2,36 +2,35 @@
 
 namespace RebelCode\WordPress\Admin\Page\FuncTest;
 
-use RebelCode\WordPress\Admin\Page\PageInterface;
+use RebelCode\WordPress\Admin\Page\PageAwareInterface as TestSubject;
 use Xpmock\TestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
 
 /**
- * Tests {@see RebelCode\WordPress\Admin\Page\PageInterface}.
+ * Tests {@see TestSubject}.
  *
  * @since [*next-version*]
  */
-class PageInterfaceTest extends TestCase
+class PageAwareInterfaceTest extends TestCase
 {
     /**
      * The name of the test subject.
      *
      * @since [*next-version*]
      */
-    const TEST_SUBJECT_CLASSNAME = 'RebelCode\\WordPress\\Admin\\Page\\PageInterface';
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\WordPress\Admin\Page\PageAwareInterface';
 
     /**
      * Creates a new instance of the test subject.
      *
      * @since [*next-version*]
      *
-     * @return PageInterface
+     * @return TestSubject|MockObject
      */
     public function createInstance()
     {
         $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
-            ->getId()
-            ->getTitle()
-            ->getContent()
+            ->getPage()
             ->new();
 
         return $mock;
@@ -46,12 +45,6 @@ class PageInterfaceTest extends TestCase
     {
         $subject = $this->createInstance();
 
-        $this->assertInstanceOf(
-            static::TEST_SUBJECT_CLASSNAME, $subject, 'Subject is not a valid instance.'
-        );
-
-        $this->assertInstanceOf(
-            'Dhii\\Data\\IdAwareInterface', $subject, 'Subject does not extend expected ancestor.'
-        );
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'Subject is not a valid instance.');
     }
 }
--- test/unit/PageInterfaceTest.php
+++ test/unit/PageInterfaceTest.php
@@ -2,34 +2,37 @@
 
 namespace RebelCode\WordPress\Admin\Page\FuncTest;
 
-use RebelCode\WordPress\Admin\Page\PageAwareInterface;
+use RebelCode\WordPress\Admin\Page\PageInterface as TestSubject;
 use Xpmock\TestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
 
 /**
- * Tests {@see RebelCode\WordPress\Admin\Page\PageAwareInterface}.
+ * Tests {@see TestSubject}.
  *
  * @since [*next-version*]
  */
-class PageAwareInterfaceTest extends TestCase
+class PageInterfaceTest extends TestCase
 {
     /**
      * The name of the test subject.
      *
      * @since [*next-version*]
      */
-    const TEST_SUBJECT_CLASSNAME = 'RebelCode\\WordPress\\Admin\\Page\\PageAwareInterface';
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\WordPress\Admin\Page\PageInterface';
 
     /**
      * Creates a new instance of the test subject.
      *
      * @since [*next-version*]
      *
-     * @return PageAwareInterface
+     * @return TestSubject|MockObject
      */
     public function createInstance()
     {
         $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
-            ->getPage()
+            ->getId()
+            ->getTitle()
+            ->getContent()
             ->new();
 
         return $mock;
@@ -44,8 +47,7 @@ class PageAwareInterfaceTest extends TestCase
     {
         $subject = $this->createInstance();
 
-        $this->assertInstanceOf(
-            static::TEST_SUBJECT_CLASSNAME, $subject, 'Subject is not a valid instance.'
-        );
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'Subject is not a valid instance.');
+        $this->assertInstanceOf('Dhii\Data\IdAwareInterface', $subject, 'Subject does not extend expected ancestor.');
     }
 }
